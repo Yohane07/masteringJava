7e9a425a5e424eb79d84a6d4ac3dd40782bc0aae
Yohane07 <yohaneendoke4@gmail.com>
2024-10-27 21:29:37 +0100

    Day 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -18,7 +18,7 @@ public class App
     }
 
     public static void addition(Integer nb1, Integer nb2){
-        out.println(nb1 + nb2);
+        out.println(nb1 - nb2);
     }
 
     public static void soustraction(Integer nb1, Integer nb2){
@@ -91,4 +91,62 @@ public class App
         scanner.close();
     }
 
+    /* Exercie 4: Verification du mot passe*/
+
+    /*
+    * String motDePasseUser;
+    * String tailleMinimum;
+    * boolean contientMajuscule = false;
+    * boolean contientMiniscule = false;
+    * boolean contientUnNombre = false;
+    *
+    * Si motDePasseUser > tailleMinimum
+    *   Si les trois booleans sont vrais
+    *       => votre mot de passe respectes les critères
+    *   Sinon si un des trois est faux
+    *       => renvoyés mdp incorrecte, il doit contenir au moins une Maj, Min , un caractère spécial et un nombre
+    * Sinon
+    *   => Votre mdp doit contenir au moins 8 caractères
+    *
+    *
+    * */
+
+    Scanner scanner = new Scanner(System.in);
+    String motDePasseUser;
+    boolean contientMajuscule = false;
+    boolean contientUnNombre = false;
+    boolean contientCaractereSpecial = false;
+
+   // String mdpAccepte = "";
+
+
+    public String verificationMotDePasse(String motDePasseUser){
+        if (motDePasseUser.length()<8){
+            return (motDePasseUser + "n'est pas assez long, votre mot de passe doit contenir au moins 8 caractères");
+        }else {
+            isContientLesBonsCaracteres(motDePasseUser);
+        }
+        return motDePasseUser;
+    }
+
+    public String isContientLesBonsCaracteres(String motDePasseUser) {
+        for (char lettre :
+             motDePasseUser.toCharArray()) {
+             if (Character.isUpperCase(lettre))
+             {
+                contientMajuscule = true;
+             }
+             else if (Character.isDigit(lettre)){
+                 contientCaractereSpecial = true;
+             }
+             else if (contientMajuscule && contientCaractereSpecial) {
+                 return ("Mot de passe accepté");
+             }
+             else {
+                 return ("Mot de passe incorrect : le mot de passe doit contenir au moins une lettre majuscule et un caractère spécial");
+             }
+        }
+
+        return null;
+    }
 }
